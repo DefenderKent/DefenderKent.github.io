1b4a190ccb871ef2db9c48789f5a0f9f6cc7241a
defender1995 <48011507+defender1995@users.noreply.github.com>
2019-12-25 20:18:03 +0700

    Еще верстка
    тестовое задание

---
+++ telmarket/js/main.js
@@ -0,0 +1,11 @@
+$(function() {
+  $(".string-slider").slick({
+    slidesToShow: 10,
+    slidesToScroll: 1
+  });
+  $(".promo-main-slider").slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots: true
+  });
+});
