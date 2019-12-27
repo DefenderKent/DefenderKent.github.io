206b1bc5772c7c84c4b6c788ba7ba1ce99cecd96
defender1995 <48011507+defender1995@users.noreply.github.com>
2019-12-27 15:47:59 +0700

    добавил магаз

---
+++ telmarket/js/main.js
@@ -0,0 +1,15 @@
+$(function() {
+  $(".string-slider").slick({
+    slidesToShow: 10,
+    slidesToScroll: 1
+  });
+  $(".promo-main-slider").slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots: true
+  });
+  $(".catalog-slider").slick({
+    slidesToShow: 4,
+    slidesToScroll: 1
+  });
+});
