366a5ac5e87c0a548fc9e5243e77865dacbccf89
DefenderKent <48011507+defender1995@users.noreply.github.com>
2020-04-10 19:47:31 +0700

    Добавил сайдбар, новые данные для отрисовки

---
+++ todo/src/components/AddButtonList/AddButtonList.jsx
@@ -0,0 +1,102 @@
+import React, { useState } from "react";
+
+import classNames from "classnames";
+import List from "./../List/List";
+
+import style from "./AddButtonList.scss";
+import closeSvg from "../../assets/img/close.svg";
+import Badge from "../Badge/Badge";
+const AddButtonList = ({ colors, onAdd }) => {
+  const [state, setState] = useState(false);
+  const [seletedColor, selectColor] = useState(colors[0].id);
+  const [inputValue, setInputValue] = useState("");
+
+  const onClose = () => {
+    setState(false);
+    setInputValue("");
+    selectColor(colors[0].id);
+  };
+
+  const addList = () => {
+    if (!inputValue) {
+      return alert("123");
+    }
+    const color = colors.filter((c) => c.id === seletedColor)[0].name;
+    onAdd({
+      id: Math.random(),
+      name: inputValue,
+      color,
+    });
+    onClose();
+  };
+
+  return (
+    <div className="add-list">
+      <List
+        onClick={() => setState(!state)}
+        items={[
+          {
+            className: "list__add-button",
+            icon: (
+              <svg
+                width="12"
+                height="12"
+                viewBox="0 0 16 16"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M8 1V15"
+                  stroke="black"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+                <path
+                  d="M1 8H15"
+                  stroke="black"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+              </svg>
+            ),
+            name: "Добавить список",
+          },
+        ]}
+      />
+      {state && (
+        <div className="add-list__popup">
+          <img
+            onClick={onClose}
+            src={closeSvg}
+            alt="Close button"
+            className="add-list__popup-close-btn"
+          />
+          <input
+            value={inputValue}
+            onChange={(e) => setInputValue(e.target.value)}
+            className="field"
+            type="text"
+            placeholder="Название списка"
+          />
+          <div className="add-list__popup-colors">
+            {colors.map((color) => (
+              <Badge
+                onClick={() => selectColor(color.id)}
+                key={color.id}
+                color={color.name}
+                className={seletedColor === color.id && "active"}
+              />
+            ))}
+          </div>
+          <button onClick={addList} className="button">
+            Добавить
+          </button>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default AddButtonList;
